e2df2c6b39471b18c252dab97a99507aa9680ff9
gweonyongil <kyi4210@daum.net>
2017-06-01 18:40:00 +0900

    view(gui)
    first upload(2017-06-01, 18:39)

---
+++ MenuBar.java
@@ -0,0 +1,34 @@
+package view;
+import javax.swing.JMenuBar;
+
+public class MenuBar extends JMenuBar{
+	private MenuFile menu_file;
+	private MenuEdit menu_edit;
+	private MenuHelp menu_help;
+	private MenuPlugins menu_plugins;
+	private MenuTools menu_tools;
+	private MenuView menu_view;
+	private MenuWindow menu_window;
+	
+	public MenuBar()
+	{
+		menu_file = new MenuFile(SMConstants.TITLE_FILEMENU);
+		add(menu_file);
+		menu_edit = new MenuEdit(SMConstants.TITLE_EDITMENU);
+		add(menu_edit);
+		menu_view = new MenuView(SMConstants.TITLE_VIEWMENU);
+		add(menu_view);
+		menu_tools = new MenuTools(SMConstants.TITLE_TOOLSMENU);
+		add(menu_tools);
+		menu_plugins = new MenuPlugins(SMConstants.TITLE_PLUGINSMENU);
+		add(menu_plugins);
+		menu_window = new MenuWindow(SMConstants.TITLE_WINDOWMENU);
+		add(menu_window);
+		menu_help = new MenuHelp(SMConstants.TITLE_HELPMENU);
+		add(menu_help);
+	}
+	public void initial()
+	{
+		
+	}
+}
