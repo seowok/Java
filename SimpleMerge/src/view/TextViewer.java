0a072af5537eb856845ede4e0f72cd2b3ba3b43d
gweonyongil <kyi4210@daum.net>
2017-06-02 22:46:58 +0900

    Text, TextViewer Updated

--- SimpleMerge/src/view/TextViewer.java
+++ SimpleMerge/src/view/TextViewer.java
@@ -12,62 +12,97 @@ import javax.swing.text.Highlighter;
 
 import model.compare.CompareLine;
 import model.compare.Line;
+import model.merge.MergeLine;
 
 
 
 public class TextViewer extends JPanel {
 	private CompareLine compareline = new CompareLine();
-	 Text lefttext;
-	private Text righttext;
+	Text lefttext;
+	Text righttext;
 	private JScrollPane lefttext_scroll;
 	private JScrollPane righttext_scroll;
 	private ArrayList<Line> left_notcomparedlines;
 	private ArrayList<Line> right_notcomparedlines;
+	private ArrayList<Line> compared_left_lines;
+	private ArrayList<Line> compared_right_lines;
 	private Document doc;
+	private MergeLine mrgline;
 
 	private ArrayList<LineColorOffset> left_linecolorlist;
 	private ArrayList<LineColorOffset> right_linecolorlist;
-	int linesetcolorindex = 0;
+	int linesetcolorindex = -1;
 
-	public TextViewer() {
+	public TextViewer() 
+	{
 		super();
+		mrgline = new MergeLine();
+		
 		setLayout(new GridLayout(1, 2, 10, 10));
 		lefttext = new Text();
+		lefttext.setText("a\nb\nc\nd\ne\n");
 		lefttext_scroll = new JScrollPane(lefttext);
 		add(lefttext_scroll);
 
 		righttext = new Text();
+		righttext.setText("f\na\nb\nc\nasasdas\nsdfssdf\na\nz\n");
 		righttext_scroll = new JScrollPane(righttext);
 		add(righttext_scroll);
 	}
-	void highliteText() {
+	void highliteText() 
+	{
 		left_notcomparedlines = compareline.constructLine(lefttext);
 		right_notcomparedlines = compareline.constructLine(righttext);
 		compareline.matchEqualLine(left_notcomparedlines, right_notcomparedlines);
 
-		ArrayList<Line> left_lines = compareline.getComparedLeft();
-		ArrayList<Line> right_lines = compareline.getComparedRight();
+		compared_left_lines = compareline.getComparedLeft();
+		compared_right_lines = compareline.getComparedRight();
 
-		lefttext = lefttext.AcceptLineColor(left_lines);
-		righttext = righttext.AcceptLineColor(right_lines);
+		lefttext = lefttext.acceptLineColor(compared_left_lines);
+		righttext = righttext.acceptLineColor(compared_right_lines);
 
 		left_linecolorlist = lefttext.getLineColorList();
 		right_linecolorlist = righttext.getLineColorList();
 	}
 
-	void nextDiff() throws BadLocationException {
+	void nextDiff() throws BadLocationException 
+	{
 		if (linesetcolorindex < left_linecolorlist.size()) {
-			lefttext.SelectLineSet(linesetcolorindex);
-			righttext.SelectLineSet(linesetcolorindex);
 			linesetcolorindex++;
+			if (linesetcolorindex < left_linecolorlist.size()) 
+			{
+				lefttext.selectLineSet(linesetcolorindex);
+				righttext.selectLineSet(linesetcolorindex);
+			}
+			else
+				linesetcolorindex--;
 		}
 	}
 
-	void prevDiff() throws BadLocationException {
-		if (linesetcolorindex >= 0) {
-			lefttext.SelectLineSet(linesetcolorindex);
-			righttext.SelectLineSet(linesetcolorindex);
+	void prevDiff() throws BadLocationException 
+	{
+		if (linesetcolorindex > 0) {
 			linesetcolorindex--;
+			lefttext.selectLineSet(linesetcolorindex);
+			righttext.selectLineSet(linesetcolorindex);
 		}
+	}
+	
+	void firstDiff() throws BadLocationException 
+	{
+		lefttext.selectLineSet(0);
+		righttext.selectLineSet(0);
+	}
+	
+	void lastDiff() throws BadLocationException 
+	{
+		lefttext.selectLineSet(left_linecolorlist.size() - 1);
+		righttext.selectLineSet(right_linecolorlist.size() - 1);
+	}
+	void shiftRight()
+	{
+		int shift_start_line = left_linecolorlist.get(linesetcolorindex).getStartLine();
+		int shift_end_line = left_linecolorlist.get(linesetcolorindex).getEndLine();
+		mrgline.moveLineTo(compared_right_lines , compared_left_lines , shift_start_line, shift_end_line);
 	}
 }
